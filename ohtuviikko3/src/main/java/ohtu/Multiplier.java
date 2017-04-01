a9d91082ef85927e4fe4910f9baf6dde8b3dc7b7
teiran <tiera.hirvonen@gmail.com>
2017-04-02 00:07:42 +0300

    tehtavat3

---
+++ ohtuviikko3/src/main/java/ohtu/Multiplier.java
@@ -0,0 +1,22 @@
+package ohtu;
+
+public class Multiplier {
+    private int value;
+    public Multiplier(int value) {
+        this.value = value;
+    }
+    public void metodi(){
+        if (true){
+            if (true) {
+                System.out.println("asdads");
+            }
+        }
+
+    }
+
+
+    public int multipliedBy(int other) {
+        return value * other;
+    }
+
+}
