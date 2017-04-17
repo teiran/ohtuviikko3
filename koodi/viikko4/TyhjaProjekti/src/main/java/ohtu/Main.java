b66fb179fd4e6e6694853aed99ac354623ad2257
teiran <tiera.hirvonen@gmail.com>
2017-04-17 19:52:59 +0300

    asda

--- koodi/viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
+++ koodi/viikko4/TyhjaProjekti/src/main/java/ohtu/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         // vaihda oma opiskelijanumerosi seuraavaan, ÄLÄ kuitenkaan laita githubiin omaa opiskelijanumeroasi
-        String studentNr = "014106411";
+        String studentNr = "";
         if ( args.length>0) {
             studentNr = args[0];
         }
